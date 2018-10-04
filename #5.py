a89ab299dcf2a4a4189cc1be617e635202945241
Kamal Nanda <42720388+KamalNanda@users.noreply.github.com>
2018-10-05 01:27:21 +0530

    Add files via upload

---
+++ #5.py
@@ -0,0 +1,16 @@
+set =['this','is','and','not','why','that','there','what','no','at']
+s=input("Enter a sentence")
+word = s.split
+for word in set:
+    if word in s:
+        s=s.replace(word,"")
+print(s)
+wd = s.split()
+print(wd)
+for x in wd:
+     print(len(x))
+
+     
+
+
+    
